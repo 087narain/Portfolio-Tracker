92e23a04cde54a82f91f0e8cace9296b7f3751ef
087narain <nair4986@gmail.com>
2025-07-08 14:52:20 +0100

    Setup the spring boot and ensured it is working

--- project/src/test/java/com/example/project/ProjectApplicationTests.java
+++ project/src/test/java/com/example/project/ProjectApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.main;
+package com.example.project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MainApplicationTests {
+class ProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
