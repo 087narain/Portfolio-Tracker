295647c52e053a543b7d7883447a0455af435f03
Narain Nair <124462314+087narain@users.noreply.github.com>
2025-07-09 14:14:48 +0100

    Implemented the Stock concrete class

---
+++ project/src/main/java/com/narain/portfoliotracker/model/Stock.java
@@ -0,0 +1,30 @@
+package com.narain.portfoliotracker.model;
+
+import java.time.LocalDateTime;
+
+public class Stock extends Asset {
+    private double currentPrice;
+
+    public Stock(String ticker, int quantity, double purchasePrice, LocalDateTime purchaseTime, String currency, double currentPrice) {
+        super(ticker, quantity, purchasePrice, purchaseTime, currency);
+        this.currentPrice = currentPrice;
+    }
+
+    public double getCurrentPrice() {
+        return this.currentPrice;
+    }
+
+    public void setCurrentPrice(double currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    @Override
+    public double getCurrentValue() {
+        return (currentPrice * quantity);
+    }
+
+    @Override
+    public String getType() {
+        return "Stock";
+    }
+}
