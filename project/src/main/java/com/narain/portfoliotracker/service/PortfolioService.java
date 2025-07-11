a76e642f37e56283ef0b01a46fe1718deb033167
Narain Nair <124462314+087narain@users.noreply.github.com>
2025-07-11 22:05:21 +0100

    Implemented business-logic to the PortfolioService

--- project/src/main/java/com/narain/portfoliotracker/service/PortfolioService.java
+++ project/src/main/java/com/narain/portfoliotracker/service/PortfolioService.java
@@ -1,5 +1,8 @@
 package com.narain.portfoliotracker.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,8 +23,60 @@ public class PortfolioService {
     public double getTotalPortfolioValue(Portfolio portfolio) {
         double totalValue = 0.0;
         for (Asset asset : portfolio.getAllAssets()) {
+            totalValue += asset.getCurrentValue();
+        }
+        return totalValue + portfolio.getBalance();
+    }
+
+    public double getPortfolioLiveValue(Portfolio portfolio) {
+        double totalValue = 0.0;
+        for (Asset asset : portfolio.getAllAssets()) {
             totalValue += getLiveValue(asset);
         }
         return totalValue + portfolio.getBalance();
+    }
+
+    public double getPortfolioValueByType(Portfolio portfolio, String assetType) {
+        double totalValue = 0.0;
+        for (Asset asset : portfolio.getAllAssets()) {
+            if (asset.getType().equals(assetType)) {
+                totalValue += asset.getCurrentValue();
+            }
+        }
+        return totalValue;
+    }
+
+    public Asset getAssetByTicker(Portfolio portfolio, String ticker) {
+        return portfolio.getAssetByTicker(ticker);
+    }
+
+    public void addAssetToPortfolio(Portfolio portfolio, Asset asset) {
+        if (portfolio.containsAsset(asset.getTicker())) {
+            Asset existingAsset = portfolio.getAssetByTicker(asset.getTicker());
+            existingAsset.setQuantity(existingAsset.getQuantity() + asset.getQuantity());
+            existingAsset.setPurchasePrice((existingAsset.getPurchasePrice() + asset.getPurchasePrice()) / 2);
+        } else {
+            portfolio.addAsset(asset);
+        }
+    }
+
+    public boolean removeAssetFromPortfolio(Portfolio portfolio, String ticker) {
+        Asset asset = portfolio.getAssetByTicker(ticker);
+        if (asset != null) {
+            portfolio.getAllAssets().remove(asset);
+            return true;
+        }
+
+        return false;
+    }
+
+    public Map<String, Double> getAssetValuesByType(Portfolio portfolio) {
+        Map<String, Double> assetValues = new HashMap<>();
+        for (Asset asset : portfolio.getAllAssets()) {
+            String type = asset.getType();
+            double value = asset.getCurrentValue();
+            assetValues.put(type, assetValues.getOrDefault(type, 0.0) + value);
+        }
+        return assetValues;
     }
 }
