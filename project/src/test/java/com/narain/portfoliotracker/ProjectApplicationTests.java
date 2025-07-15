e55fcaf449e9e80723a447b038225765c147181e
Narain Nair <124462314+087narain@users.noreply.github.com>
2025-07-15 10:40:09 +0100

    Model imports for test

--- project/src/test/java/com/narain/portfoliotracker/ProjectApplicationTests.java
+++ project/src/test/java/com/narain/portfoliotracker/ProjectApplicationTests.java
@@ -1,5 +1,7 @@
 package com.narain.portfoliotracker;
 
+import com.narain.portfoliotracker.model.Asset;
+import com.narain.portfoliotracker.model.Portfolio;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
