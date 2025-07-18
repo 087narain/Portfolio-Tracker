89a3d421c2e6d083b7166f04cbf6b5a0d21567c4
Narain Nair <124462314+087narain@users.noreply.github.com>
2025-07-18 16:14:59 +0100

    Implemented header component.

---
+++ project/client/src/components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="bg-white shadow-md rounded-lg p-6 w-full max-w-xl text-center">
+        <h1 className="text-2xl font-bold">📊 Portfolio Tracker</h1>
+        </header>
+    );
+}
