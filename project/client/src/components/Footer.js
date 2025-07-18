f2bf55a2733e4a6b0b897b3017d431dc08bc2870
Narain Nair <124462314+087narain@users.noreply.github.com>
2025-07-18 16:20:55 +0100

    Footer component implemented.

---
+++ project/client/src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="text-center text-gray-500 text-sm mt-6">
+            © {new Date().getFullYear()} Narain's Portfolio Tracker
+        </footer>
+    );
+}
