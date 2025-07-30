e2b06fffd6d6446d00e4b59e5eb0b12addeacd15
Narain Nair <124462314+087narain@users.noreply.github.com>
2025-07-30 22:59:04 +0100

    Outlined getters and setters for the DTO.

---
+++ project/src/main/java/com/narain/portfoliotracker/dto/CreatePortfolio.java
@@ -0,0 +1,31 @@
+package com.narain.portfoliotracker.dto;
+
+public class CreatePortfolio {
+    private String portfolioName;
+    private double balance;
+    private String currency;
+
+    public String getPortfolioName() {
+        return portfolioName;
+    }
+
+    public void setPortfolioName(String portfolioName) {
+        this.portfolioName = portfolioName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
