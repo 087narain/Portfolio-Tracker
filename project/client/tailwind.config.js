2e97a5bef9cc52bedea29b7e1ce1a2e5d53c9d7e
Narain Nair <124462314+087narain@users.noreply.github.com>
2025-08-08 12:44:01 +0100

    Enhanced Tailwind to allow for a depth effect.

--- project/client/tailwind.config.js
+++ project/client/tailwind.config.js
@@ -7,8 +7,19 @@ export const theme = {
     colors: {
       accentBlue: '#1DA1F2',
       accentGreen: '#00B894',
-      darkbg: '#121212',
-      darkcard: '#1E1E2F',
+
+      darkbg: '#121212',        
+      darkcard: '#1E1E2F',       
+
+      darkGray900: '#121212',    
+      darkGray800: '#1A1A2E',
+      darkGray700: '#23233F',
+      darkGray600: '#2D2D53',
+      darkGray500: '#3B3B6F',
+      darkGray400: '#4E4E8A',
+      darkGray300: '#6666A6',
+      darkGray200: '#8484C2',
+      darkGray100: '#A5A5DD',    
     },
     spacing: {
       '72': '18rem',
