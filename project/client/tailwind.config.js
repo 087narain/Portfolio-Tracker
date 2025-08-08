08043e5bca299f5b8d75109115e45257caab3cf6
Narain Nair <124462314+087narain@users.noreply.github.com>
2025-08-08 11:42:33 +0100

    Included a Tailwind module for the intended colour palette.

--- project/client/tailwind.config.js
+++ project/client/tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}"
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  theme: {
+    extend: {
+      colors: {
+        accentBlue: '#1DA1F2',
+        accentGreen: '#00B894',
+        darkbg: '#121212',
+        darkcard: '#1E1E2F',
+      },
+      spacing: {
+        '72': '18rem', // you can add custom spacing too
+      }
+    }
+  },
+  plugins: [],
+}
